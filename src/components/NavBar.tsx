f9c0b331838dfe26ea7c578f1c9288b6826b73b4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-24 17:05:04 +0900

    display icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,12 +15,20 @@ const useStyles = makeStyles(() => ({
 const NavBar: React.FC = () => {
   const classes = useStyles()
 
+  const logout = (): void => {}
+
   return (
     <Appbar position="static">
       <Toolbar>
+        <button className="logo">
+          <FaYoutube />
+        </button>
         <Typography variant="h5" className={classes.title}>
           Youtube App
         </Typography>
+        <button className="logOut" onClick={() => logout()}>
+          <FiLogOut />
+        </button>
       </Toolbar>
     </Appbar>
   )
