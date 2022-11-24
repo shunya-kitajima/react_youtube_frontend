6fcad8e2c6533f76817d4b27f4d241ebc2bcdfc8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-24 16:18:45 +0900

    modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
   overrides: [],
@@ -17,5 +17,5 @@ module.exports = {
       'error',
       { checksVoidReturn: { attributes: false } },
     ],
-  }
+  },
 }
