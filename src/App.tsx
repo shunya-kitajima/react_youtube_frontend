d634f9752c68aa63b7b2bb777ef99650ddaca1b3
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-24 16:46:51 +0900

    set MuiThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -20,22 +20,9 @@ const theme = createTheme({
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div>main</div>
+    </MuiThemeProvider>
   )
 }
 
