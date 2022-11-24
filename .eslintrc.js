af7980c2c9703e12f85844c5015769dcef394976
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-24 16:11:22 +0900

    modify lines

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,15 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'prettier',
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
     '@typescript-eslint/no-misused-promises': [
       'error',
