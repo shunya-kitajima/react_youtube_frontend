41021a6030e9d2434a724f29f6af92df59e95e2c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-25 13:36:06 +0900

    modify

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,12 +2,8 @@ export interface InitialState {
   isLoading: boolean
   isLoginView: boolean
   error: string
-  credentialsReg: {
-    email: string
-    password: string
-  }
   credentialsLog: {
-    username: string
+    email: string
     password: string
   }
 }
