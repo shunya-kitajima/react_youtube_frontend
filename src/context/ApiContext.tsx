c988e5a1791665a9adb834789980e05a382d2091
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-25 15:05:58 +0900

    import modules

---
+++ src/context/ApiContext.tsx
@@ -0,0 +1,9 @@
+import React, { createContext, useState, useEffect } from 'react'
+import { withCookies } from 'react-cookie'
+import axios from 'axios'
+
+const ApiContext: React.FC = () => {
+  return <div></div>
+}
+
+export default ApiContext
