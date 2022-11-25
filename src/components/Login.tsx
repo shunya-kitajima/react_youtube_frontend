28f3db18421169baab0587275d17e713a22f54d0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-25 11:50:16 +0900

    create useStyles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -18,6 +18,40 @@ import {
   TOGGLE_MODE,
 } from './actionTypes'
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  span: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: 'teal',
+    cursor: 'pointer',
+  },
+  spanError: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: 'fuchsia',
+    marginTop: 10,
+  },
+}))
+
 const Login: React.FC = (props: any) => {
   return <div></div>
 }
