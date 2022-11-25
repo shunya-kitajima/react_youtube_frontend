d95685ea7240b44e41403dfc27af6e9bb81bffd0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-25 18:08:24 +0900

    set useEffect

--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -9,10 +9,37 @@ const ApiContextProvider: React.FC = (props: any) => {
   const token = props.cookies.get('jwt-token') as string
   const [videos, setVideos] = useState<Video[]>([])
   const [title, setTitle] = useState('')
-  const [video, setVideo] = useState(null)
-  const [thum, setThum] = useState(null)
-  const [selectedVideo, setSelectedVideo] = useState(null)
+  const [video, setVideo] = useState<File>(
+    new File(['dummy'], 'dummy.txt', { type: 'text/plain' })
+  )
+  const [thum, setThum] = useState<File>(
+    new File(['dummy'], 'dummy.txt', { type: 'text/plain' })
+  )
+  const [selectedVideo, setSelectedVideo] = useState<Video>({
+    id: '',
+    title: '',
+    video: new File(['dummy'], 'dummy.txt', { type: 'text/plain' }),
+    thum: new File(['dummy'], 'dummy.txt', { type: 'text/plain' }),
+    like: 0,
+    dislike: 0,
+  })
   const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  useEffect(() => {
+    const getVideos = async (): Promise<void> => {
+      try {
+        const res = await axios.get('http://127.0.0.1:8000/api/videos/', {
+          headers: {
+            Authorization: `JWT ${token}`,
+          },
+        })
+        setVideos(res.data)
+      } catch (err: any) {
+        console.log(err.message)
+      }
+    }
+    void getVideos()
+  }, [token])
 
   return <ApiContext.Provider value={{}}>{props.children}</ApiContext.Provider>
 }
