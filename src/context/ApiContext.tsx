3d03396b4f07c36e49b1c40028532e4ff9f82808
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-25 15:22:14 +0900

    set states

--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -6,6 +6,14 @@ import { Video } from '../types'
 export const ApiContext = createContext()
 
 const ApiContextProvider: React.FC = (props: any) => {
+  const token = props.cookies.get('jwt-token') as string
+  const [videos, setVideos] = useState<Video[]>([])
+  const [title, setTitle] = useState('')
+  const [video, setVideo] = useState(null)
+  const [thum, setThum] = useState(null)
+  const [selectedVideo, setSelectedVideo] = useState(null)
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
   return <ApiContext.Provider value={{}}>{props.children}</ApiContext.Provider>
 }
 
