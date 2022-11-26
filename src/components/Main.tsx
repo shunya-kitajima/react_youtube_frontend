caf8e132e8cdfea73e42c2b55e3b18f84908c312
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-26 15:13:43 +0900

    set useStyles and contexts and customstyles

---
+++ src/components/Main.tsx
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react'
+import Modal from 'react-modal'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+import Fab from '@material-ui/core/Fab'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import IconButton from '@material-ui/core/IconButton'
+import AddIcon from '@material-ui/icons/Add'
+import { IoMdClose } from 'react-icons/io'
+import { RiUploadCloud2Line } from 'react-icons/ri'
+import { FaViadeo } from 'react-icons/fa'
+import { BsImage } from 'react-icons/bs'
+import { ApiContext } from '../context/ApiContext'
+import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
+
+const useStyles = makeStyles(() => ({
+  container: {
+    textAlign: 'center',
+  },
+  grid: {
+    justifyContent: 'center',
+  },
+}))
+
+const Main: React.FC = () => {
+  const classes = useStyles()
+  Modal.setAppElement('#root')
+  const {
+    title,
+    setTitle,
+    video,
+    setVideo,
+    thum,
+    setThum,
+    modalIsOpen,
+    setModalIsOpen,
+    createVideo,
+  } = useContext(ApiContext)
+  const customStyles = {
+    content: {
+      top: '30%',
+      left: '43%',
+      right: 'auto',
+      bottom: 'auto',
+    },
+  }
+
+  return <div></div>
+}
+
+export default Main
