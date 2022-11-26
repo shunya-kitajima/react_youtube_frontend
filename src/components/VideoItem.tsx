ab91e91e99e8b02430807de8121a10e08a53f9a5
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-26 15:04:11 +0900

    create template

---
+++ src/components/VideoItem.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const VideoItem: React.FC = () => {
+  return <div></div>
+}
+
+export default VideoItem
---
+++ src/components/VideoList.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const VideoList: React.FC = () => {
+  return <div></div>
+}
+
+export default VideoList
