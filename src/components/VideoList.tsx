28f984688bb650a10f9990a9a651ae9f925d9035
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-27 20:48:42 +0900

    import modules and set context

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Grid from '@material-ui/core/Grid'
+import { ApiContext } from '../context/ApiContext'
+import VideoItem from './VideoItem'
 
 const VideoList: React.FC = () => {
-  return <div></div>
+  const { videos } = useContext(ApiContext)
+  const videoList = videos.map((video) => (
+    <VideoItem key={video.id} video={video} />
+  ))
+
+  return <Grid></Grid>
 }
 
 export default VideoList
