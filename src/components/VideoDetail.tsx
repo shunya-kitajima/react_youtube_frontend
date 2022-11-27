704a320884d2d4521dac504abca364d708cbbd8d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-27 21:30:15 +0900

    import modules

--- src/components/VideoDetail.tsx
+++ src/components/VideoDetail.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ReactPlayer from 'react-player'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+import Fab from '@material-ui/core/Fab'
+import Typography from '@material-ui/core/Typography'
+import DeleteIcon from '@material-ui/icons/Delete'
+import { AiFillLike, AiFillDislike } from 'react-icons/ai'
+import { IoLogoYoutube } from 'react-icons/io'
+import { ApiContext } from '../context/ApiContext'
+import { Video } from '../types'
+
+export interface Props {
+  video: Video
+}
 
 const VideoDetail: React.FC = () => {
+  const {} = useContext(ApiContext)
+
   return <div></div>
 }
 
