87f7adb846bebb01cd643515a02f5fafa5545073
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-27 20:24:36 +0900

    import modules

--- src/components/VideoItem.tsx
+++ src/components/VideoItem.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import Typography from '@material-ui/core/Typography'
+import { ApiContext } from '../context/ApiContext'
 
 const VideoItem: React.FC = () => {
   return <div></div>
