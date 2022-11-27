322227b41773fbc4c908ad82e751b9ecbb5fc876
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-27 20:33:32 +0900

    display thum and title by card

--- src/components/VideoItem.tsx
+++ src/components/VideoItem.tsx
@@ -6,9 +6,43 @@ import CardContent from '@material-ui/core/CardContent'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import Typography from '@material-ui/core/Typography'
 import { ApiContext } from '../context/ApiContext'
+import { Video } from '../types'
 
-const VideoItem: React.FC = () => {
-  return <div></div>
+export interface Props {
+  video: Video
+}
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    position: 'relative',
+    display: 'flex',
+    marginBottom: 15,
+  },
+  cardcont: {
+    padding: theme.spacing(1),
+  },
+}))
+
+const VideoItem: React.FC<Props> = ({ video }) => {
+  const classes = useStyles()
+  const { setSelectedVideo } = useContext(ApiContext)
+
+  return (
+    <Card className={classes.card} onClick={() => setSelectedVideo(video)}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="thumnail"
+          height="200"
+          image={video.thum}
+        >
+          <CardContent className={classes.cardcont}>
+            <Typography variant="h6">{video.title}</Typography>
+          </CardContent>
+        </CardMedia>
+      </CardActionArea>
+    </Card>
+  )
 }
 
 export default VideoItem
